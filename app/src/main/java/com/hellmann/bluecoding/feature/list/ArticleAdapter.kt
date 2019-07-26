ec654f174b55e0f8a32ec0719f1cafacaf036dbf
Maicon Hellmann <maiconhellmann@gmail.com>
2019-07-26 10:47:29 +0200

    Show a favorite button/status Added resources for the favorite button Movie list layout improvements

--- app/src/main/java/com/hellmann/bluecoding/feature/list/ArticleAdapter.kt
+++ app/src/main/java/com/hellmann/bluecoding/feature/list/ArticleAdapter.kt
@@ -4,11 +4,11 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.hellmann.bluecoding.R
 import com.hellmann.bluecoding.domain.entity.Movie
+import com.hellmann.bluecoding.util.extensions.getString
 import com.hellmann.bluecoding.util.extensions.inflate
 import com.hellmann.bluecoding.util.extensions.load
-import com.hellmann.bluecoding.util.extensions.visible
 import kotlinx.android.synthetic.main.fragment_movie_item.view.image
-import kotlinx.android.synthetic.main.fragment_movie_item.view.subtitle
+import kotlinx.android.synthetic.main.fragment_movie_item.view.rating
 import kotlinx.android.synthetic.main.fragment_movie_item.view.title
 
 class MoviesAdapter : RecyclerView.Adapter<MoviesAdapter.ViewHolder>() {
@@ -20,10 +20,10 @@ class MoviesAdapter : RecyclerView.Adapter<MoviesAdapter.ViewHolder>() {
 
         fun bind(movie: Movie) = with(itemView) {
             title.text = movie.title
-            subtitle.text = movie.homepage
-            subtitle.visible(movie.homepage?.isNotEmpty() ?: false)
+            rating.text = getString(R.string.fragment_movie_rating_average, movie.voteAverage)
             image.load(movie.posterPath)
 
+            //TODO favourite status
 //            if (movie.url != null) {
 //                movie.url?.let {
 //                    val directions = MovieListFragmentDirections.actionOpenWebview(it)
---
+++ app/src/main/java/com/hellmann/bluecoding/util/extensions/ViewHolder.kt
@@ -0,0 +1,15 @@
+package com.hellmann.bluecoding.util.extensions
+
+import androidx.annotation.StringRes
+import androidx.recyclerview.widget.RecyclerView
+
+/*
+ * This file is part of BlueCodingEvaluationIMDB.
+ * 
+ * Created by maiconhellmann on 26/07/2019
+ * 
+ * (c) 2019 
+ */
+fun RecyclerView.ViewHolder.getString(@StringRes resId: Int, vararg: Any): String {
+    return this.itemView.context.getString(resId, vararg)
+}
