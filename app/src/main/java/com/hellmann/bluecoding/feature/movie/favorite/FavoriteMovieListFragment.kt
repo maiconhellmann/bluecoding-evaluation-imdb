f6519a746526a0bace3426c105754659943cdd23
Maicon Hellmann <maiconhellmann@gmail.com>
2019-07-26 14:23:55 +0200

    created movie tabs skeletom

---
+++ app/src/main/java/com/hellmann/bluecoding/feature/movie/favorite/FavoriteMovieListFragment.kt
@@ -0,0 +1,9 @@
+package com.hellmann.bluecoding.feature.movie.favorite
+
+/*
+ * This file is part of BlueCodingEvaluationIMDB.
+ * 
+ * Created by maiconhellmann on 26/07/2019
+ * 
+ * (c) 2019 
+ */class FavoriteMovieListFragment {}
